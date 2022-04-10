0baf10b274d59d0207a82f656acbd564eae41298
barunranjan <vrnranjan22@gmail.conm>
2022-04-10 23:16:20 +0530

    create an array datastructure from begning

---
+++ data-structure/array/creatingArray.js
@@ -0,0 +1,34 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+  pop() {
+    const lastitem = this.data[this.length - 1];
+    delete this.data.lastitem;
+    this.length--;
+    return lastitem;
+  }
+  delete(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+const newArr = new MyArray();
+newArr.push("hello");
+newArr.push("world");
+newArr.push(2022);
+newArr.delete(0);
+
+console.log(newArr);
